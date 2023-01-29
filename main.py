c58855cb2a7559f8601361b9fdedfb0067317ad8
unknown <henrique.ph935@gmail.com>
2023-01-29 13:13:13 -0300

    Cria login.html

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-lista_usuarios = ['henrique', 'santos', 'paulao']
+lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route("/contato")
+@app.route('/contato')
 def contato():
     return render_template('contato.html')
 
-@app.route("/usuarios")
+@app.route('/usuarios')
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
